665b419cf7ef824652c65d292e428cf8a899a1a3
Серов Сергей Олегович <newnpr.sserov@tcsbank.ru>
2016-05-05 18:59:25 +0300

    jinn

--- src/main/java/uz/utils/AutoIt.java
+++ src/main/java/uz/utils/AutoIt.java
@@ -29,9 +29,12 @@ public class AutoIt {
 	 * Выбираем сертификат
 	 */
 	 public void selectCertInJinnClient(){
+		 System.out.println("Жду появление окна: Jinn-Client");
 		 control.winWait("Jinn-Client", "", 20); //[Title:Jinn-Client]
 		if(control.winExists("Jinn-Client")){
+			System.out.println("Появилось окно: Jinn-Client");
 			control.winActivate("Jinn-Client");
+			System.out.println("Сделал окно активным: Jinn-Client.");
 			/*
 			control.controlClick("", "Обновить", "");
 			try {
@@ -41,12 +44,18 @@ public class AutoIt {
 			}
 			*/
 			control.controlClick("Jinn-Client", "", "[CLASS:Edit; INSTANCE:1]");
+			System.out.println("Клик на поле ввода в окне: Jinn-Client.");
 			control.controlSend("Jinn-Client","","[CLASS:Edit; INSTANCE:1]","12345678");
+			System.out.println("Ввел текст в поле ввода в окне: Jinn-Client.");
 			control.controlClick("Jinn-Client", "", "[CLASS:Button; INSTANCE:2]");
+			System.out.println("Нажал кнопку в окне: Jinn-Client.");
+			System.out.println("Жду исчезновение окна: Jinn-Client.");
 			Boolean isClose = control.winWaitClose("Jinn-Client", "", 20);
 			if(!isClose){
+				System.out.println("Окно не исчезло: Jinn-Client.");
 				MatcherAssert.assertThat("Окно Jinn-Clien не закрылось", false);
 			}
+			System.out.println("Окно исчезло: Jinn-Client.");
 		}else{
 			System.out.println("Окно Jinn-Client для выбора сертификата не появилось");
 			//MatcherAssert.assertThat("Окно Jinn-Client. Просмотр документа перед формированием подписи не появилось", false);
@@ -54,15 +63,22 @@ public class AutoIt {
 	 }
 	 
 	 public void signInJinnClient(){
-		 control.winWait("Jinn-Client. Просмотр документа перед формированием подписи", "", 20); //[Title:Jinn-Client]
+		 System.out.println("Жду появление окна: Jinn-Client. Просмотр документа перед формированием подписи не появилось");
+		 control.winWait("Jinn-Client. Просмотр документа перед формированием подписи", "", 20);
 		if(control.winExists("Jinn-Client. Просмотр документа перед формированием подписи")){
+			System.out.println("Появилось окно: Jinn-Client. Просмотр документа перед формированием подписи не появилось");
 			control.winActivate("Jinn-Client. Просмотр документа перед формированием подписи");
+			 System.out.println("Сделал окно активным: Jinn-Client. Просмотр документа перед формированием подписи не появилось");
 			control.controlClick("Jinn-Client. Просмотр документа перед формированием подписи", "", "[CLASS:Button; INSTANCE:1]");
-			
+			 System.out.println("Нажал кнопку в окне: Jinn-Client. Просмотр документа перед формированием подписи не появилось");
+			 System.out.println("Жду исчезновение окна: Jinn-Client. Просмотр документа перед формированием подписи не появилось");
 			Boolean isClose = control.winWaitClose("Jinn-Client. Просмотр документа перед формированием подписи", "", 20);
+			
 			if(!isClose){
+				System.out.println("Окно не исчезло: Jinn-Client. Просмотр документа перед формированием подписи не появилось");
 				MatcherAssert.assertThat("Jinn-Client. Просмотр документа перед формированием подписи", false);
 			}
+			System.out.println("Окно исчезло: Jinn-Client. Просмотр документа перед формированием подписи не появилось");
 		}else{
 			System.out.println("Окно Jinn-Client. Просмотр документа перед формированием подписи не появилось");
 			//MatcherAssert.assertThat("Окно Jinn-Client. Просмотр документа перед формированием подписи не появилось", false);
