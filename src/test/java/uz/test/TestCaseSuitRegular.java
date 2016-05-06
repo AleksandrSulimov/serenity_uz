44c02e642c2248678b311849616667bcd64529f2
nprogs.ekuzmina <ekuzmina@newnpr.com>
2016-05-06 19:51:19 +0300

    ddfdfdfffffffffffffffffffff

--- src/test/java/uz/test/TestCaseSuitRegular.java
+++ src/test/java/uz/test/TestCaseSuitRegular.java
@@ -5,14 +5,14 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+	CheckAndRemoveExistPZ.class,
+	CheckAndRemoveExistPG.class, 
 	Create_PPZ_200_Regular.class,
 	Approval_PPZ.class,
-	CheckAndRemoveExistPZ.class,
 	Create_PZ.class,
 	Approval_PZ.class,
 	Create_PPG_Regular.class,
 	Approval_PPG.class,
-	CheckAndRemoveExistPG.class, 
 	Create_PG.class, 
 	Approval_PG.class
 	})
